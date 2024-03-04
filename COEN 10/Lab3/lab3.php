94067f7d45a735f7d1014763e312c07adf413fa2
vaniaggarwal <52869093+vaniaggarwal@users.noreply.github.com>
2024-03-03 21:07:26 -0800

    Create lab3.php
    game to guess the number

---
+++ COEN 10/Lab3/lab3.php
@@ -0,0 +1,12 @@
+<html>
+		<h1>
+			Guess the Number Game!
+		</h1>
+		<p>
+			Input a number from 1 to 20:
+		</p>
+
+		<form action="lab3-2.php" method = "POST">
+			<input type= "number" name="number" value="<?php echo $number; ?>"/>
+		</form>
+</html>
