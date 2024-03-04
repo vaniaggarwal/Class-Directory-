e746e23f35d18003d9b92009ea42cd0463397b50
vaniaggarwal <52869093+vaniaggarwal@users.noreply.github.com>
2024-03-03 22:49:58 -0800

    update

--- COEN 10/LAB5/lab5.c
+++ COEN 10/LAB5/lab5.c
@@ -6,28 +6,33 @@
  */
 #include <stdio.h>
 #include <stdlib.h> // for rand() and srand()
-#include <time.h> // time()
-int main() {
-    int number1, number2, product; // declare variables
-    int i; // declare variable
-    int counter = 0; // declare and initialize counter
-    srand((int) time (NULL)); // seeds 
-    for (i=0; i < 10; i++) { // start of for loop repeated 10 times
-	      int number1 = rand() % 13; // generates first random number between 0 and 12
-	      int number2 = rand() % 13; // generates second random number between 0 and 12
-	      printf("Enter the product of the following two numbers: %d x %d\n", number1, number2); // prompts user input 
-	      scanf("%d", &product); // user inputs product
+#include <time.h>	// time()
+int main()
+{
+	int number1, number2, product; // declare variables
+	int i;						   // declare variable
+	int counter = 0;			   // declare and initialize counter
+	srand((int)time(NULL));		   // seeds
+	for (i = 0; i < 10; i++)
+	{																						   // start of for loop repeated 10 times
+		int number1 = rand() % 13;															   // generates first random number between 0 and 12
+		int number2 = rand() % 13;															   // generates second random number between 0 and 12
+		printf("Enter the product of the following two numbers: %d x %d\n", number1, number2); // prompts user input
+		scanf("%d", &product);																   // user inputs product
 
-    	if (number1 * number2 == product) {  // start of if statement
-    	    printf("Correct! Good Job!\n"); // outputs if correct 
-    	    counter += 1; // counter = counter + 1
-    	} else {
-    	    printf("Oops! That's Wrong. The Correct Answer is: "); 
-    	    product = number1 * number2;
-    	    printf("%d\n", product); // outputs correct answer 
-    	  }
-  	}	
-  
-      counter = counter * 10;
-      printf("Score (in percent): %d%%", counter); // outputs score in percentage
+		if (number1 * number2 == product)
+		{									// start of if statement
+			printf("Correct! Good Job!\n"); // outputs if correct
+			counter += 1;					// counter = counter + 1
+		}
+		else
+		{
+			printf("Oops! That's Wrong. The Correct Answer is: ");
+			product = number1 * number2;
+			printf("%d\n", product); // outputs correct answer
+		}
+	}
+
+	counter = counter * 10;
+	printf("Score (in percent): %d%%", counter); // outputs score in percentage
 }
